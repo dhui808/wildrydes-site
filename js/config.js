065683253800eca2f26cab3b67939603c6860292
Danny Hui <39434445+dhui808@users.noreply.github.com>
2022-05-24 11:45:46 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_xNrgEBb3N', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1o2rq4ouo4c80lelb8hhu5i524', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_8YflySxfd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2q17qvf55mbal7uvlgbp81cs0h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
